93f971e66d4e3805c0d23c35e82ece9aea7e40f6
mayconserra-sketch <mayconserra@hotmail.com>
2026-03-01 21:23:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ initDB();
 // CONFIG EXPRESS
 // =======================
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
